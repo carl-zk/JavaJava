b6eef4511e8b7529fca513663fd2bf9136668df9
carl <xinfeng.zhao2@wework.com>
2019-08-01 14:09:32 +0800

    web

--- web/src/main/java/com/hero/web/controller/UserController.java
+++ web/src/main/java/com/hero/web/controller/UserController.java
@@ -5,7 +5,14 @@ import com.hero.web.constant.ServiceException;
 import com.hero.web.vo.UserVO;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
